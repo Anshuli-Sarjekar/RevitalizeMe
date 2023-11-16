97a97855ffa90997aa82eb4846ca6d566300ed34
Anshuli Sarjekar <sarjekaranshuli@gmail.com>
2023-11-16 16:14:27 +0530

    Add port and host

--- app.py
+++ app.py
@@ -553,7 +553,7 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, port = 8000, host = "0.0.0.0")
 
 with app.app_context():
     db.create_all()  # Create database tables
