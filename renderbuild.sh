8acd667bc5567e6637c095bcd4e253f62fa07f57
Anshuli Sarjekar <sarjekaranshuli@gmail.com>
2023-11-16 20:13:46 +0530

    Added .yaml file and modified .sh file

--- renderbuild.sh
+++ renderbuild.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#!/bin/bash
+
+# Upgrade Python
+sudo add-apt-repository ppa:deadsnakes/ppa
+
+sudo apt update
+sudo apt install python3.11=3.11.5*
+
+# Upgrade pip and install requirements
+# /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip && /opt/render/project/src/.venv/bin/python -m pip install -r /opt/render/project/src/requirements.txt
 
 echo "Updating pip..."
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
